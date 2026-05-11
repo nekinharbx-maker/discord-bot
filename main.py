9f985da120834930fe1cca58c81f422c79ff61b7
nekinharbx-maker <nekinharbx@gmail.com>
2026-05-11 05:05:24 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = os.environ["TOKEN"]
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=".", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot online: {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+bot.run(TOKEN)
